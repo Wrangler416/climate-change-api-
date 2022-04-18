f8da98f45055796eb62bec0a801435c818ffc18a
Wrangler416 <kara.temple@bryanuniversity.edu>
2022-04-18 17:01:51 -0500

    removenodmod

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ app.get('/news', (req, res) => {
  res.json(articles)
 })
 
-app.get('/news/:newspaperId', async(req, res)=> {
+app.get('/news/:newspaperId',(req, res)=> {
   const newspaperId = req.params.newspaperId
   const newspaperAddress = newspapers.filter(newspaper => newspaper.name == newspaperId)[0].address
   const newspaperBase = newspapers.filter(newspaper => newspaper.name == newspaperId)[0].base
